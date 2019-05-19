9876fefd1534b3040d96d53510533edff1c57442
gabrielvasile <gabriell.vasile@gmail.com>
2019-05-19 15:51:13 +0800

    green

--- server.sh
+++ server.sh
@@ -1,3 +1,4 @@
 curl https://raw.githubusercontent.com/gabycasper007/megasoftcalculator/master/docker-compose.yml > docker-compose.yml
 docker-compose pull
-docker-compose up --force-recreate --build -d
+docker-compose down
+docker-compose up -d
